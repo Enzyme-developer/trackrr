d246d8c0f57ff7e35ce4f8f6ead87392c155cfa3
Ayo <dannyhopkins20003@gmail.com>
2024-03-03 14:47:49 +0100

    connected database

--- app/lib/actions/userAction.ts
+++ app/lib/actions/userAction.ts
@@ -5,7 +5,6 @@ import User from "../database/models/userModel";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
-// CREATE
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
@@ -18,7 +17,6 @@ export async function createUser(user: CreateUserParams) {
   }
 }
 
-// READ
 export async function getUserById(userId: string) {
   try {
     await connectToDatabase();
@@ -33,7 +31,6 @@ export async function getUserById(userId: string) {
   }
 }
 
-// UPDATE
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
     await connectToDatabase();
@@ -50,42 +47,19 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
   }
 }
 
-// DELETE
 export async function deleteUser(clerkId: string) {
   try {
     await connectToDatabase();
-
-    // Find user to delete
     const userToDelete = await User.findOne({ clerkId });
 
     if (!userToDelete) {
       throw new Error("User not found");
     }
 
-    // Delete user
     const deletedUser = await User.findByIdAndDelete(userToDelete._id);
     revalidatePath("/");
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// USE CREDITS
-export async function updateCredits(userId: string, creditFee: number) {
-  try {
-    await connectToDatabase();
-
-    const updatedUserCredits = await User.findOneAndUpdate(
-      { _id: userId },
-      { $inc: { creditBalance: creditFee } },
-      { new: true }
-    );
-
-    if (!updatedUserCredits) throw new Error("User credits update failed");
-
-    return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
     handleError(error);
   }
