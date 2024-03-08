6e444e092a067fa65ba50e6b2162fa1d3d925b09
Ayo <dannyhopkins20003@gmail.com>
2024-03-08 18:29:36 +0100

    added layout to auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function Auth({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="p-4 w-full min-h-screen flex items-center justify-center">
+    <div className="p-4 md:p-6 w-full min-h-screen flex items-center justify-center">
       {children}
     </div>
   );
